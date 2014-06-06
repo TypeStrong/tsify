4fc9284c96b6d231399d6e40005608e25a8f7d71
Greg Smith <smr.kyu@gmail.com>
2014-06-06 15:22:48 -0500

    Made sourcemap test independent of repo directory
    The repo's absolute local path becomes part of the generated sourcemap,
    so the test was failing when that absolute path was different

--- test.js
+++ test.js
@@ -2,32 +2,35 @@ var test = require('tape');
 
 var ansidiff = require('ansidiff');
 var browserify = require('browserify');
+var convert = require('convert-source-map');
 var es = require('event-stream');
 var fs = require('fs');
+var path = require('path');
 var Q = require('q');
 
 test('no arguments', function (t) {
-	runTest(t, {}, {}, 'test/expected.js');
+	var expected = fs.readFileSync('test/expected.js').toString();
+	runTest(t, {}, {}, expected);
 });
 test('--sourcemap', function (t) {
-	runTest(t, { sourcemap: true }, { debug: true }, 'test/expectedSourcemap.js');
+	var expected = fs.readFileSync('test/expectedSourcemap.js').toString();
+	var sourcemap = convert.fromSource(expected);
+	var sources = sourcemap.getProperty('sources');
+	sources[0] = path.resolve(__dirname, 'node_modules/browserify/node_modules/browser-pack/_prelude.js');
+	sourcemap.setProperty('sources', sources);
+	expected = expected.replace(convert.commentRegex, sourcemap.toComment());
+	runTest(t, { sourcemap: true }, { debug: true }, expected);
 });
 
-function runTest(t, tsifyOptions, bundleOptions, expectedFile) {
+function runTest(t, tsifyOptions, bundleOptions, expected) {
 	t.plan(1);
 
-	var expectedDefer = Q.defer();
 	var actualDefer = Q.defer();
-
-	fs.readFile(expectedFile, function (err, data) { expectedDefer.resolve(data.toString()); });
 	browserify({ extensions: ['.ts'] })
 		.add('./test/x.ts')
 		.plugin('./index.js', tsifyOptions)
 		.bundle(bundleOptions)
-		.pipe(es.wait(function (err, data) { actualDefer.resolve(data); }));
-
-	Q.all([expectedDefer.promise, actualDefer.promise])
-		.spread(function (expected, actual) {
+		.pipe(es.wait(function (err, actual) { 
 			actual = actual.replace(/\r\n/g, '\n'); // fix CRLFs on Windows; the expected output uses LFs
 			if (expected === actual) {
 				t.pass('Test passed');
@@ -35,5 +38,5 @@ function runTest(t, tsifyOptions, bundleOptions, expectedFile) {
 				console.log(ansidiff.lines(expected, actual));
 				t.fail('Test failed');
 			}
-		});
+		}));
 }

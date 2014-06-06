9dbf166cda936ec4c477e4066b0c43f409e68e40
Greg Smith <smr.kyu@gmail.com>
2014-06-06 17:04:55 -0500

    Refactored Browserify dependency out of Tsifier
    The Tsifier module now only requires its own options, and has no
    dependency on Browserify.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Tsifier = require('./lib/Tsifier');
 
 function tsify(b, opts) {
-	var tsifier = new Tsifier(b, opts);
-	b.on('bundle', tsifier.compileAndCacheFiles.bind(tsifier));
+	var tsifier = new Tsifier(opts);
+	tsifier.on('error', function (error) { b.emit('error', error); });
+	b.on('bundle', function () { tsifier.compileAndCacheFiles(b._entries); });
 	b.transform(tsifier.transform.bind(tsifier));
 };
 

4655390b801d86c3b96d1ecf520e261679e0e60e
Nico Rehwaldt <git_nikku@nixis.de>
2014-08-26 01:00:19 +0200

    fix(plugin): properly handle late added files
    This commit adds proper handling for late added files supported from
    browserify 5.x on.
    
    It is backwards compatible with browserify 4.x and the existing 5.x
    behavior (which supports options.entries) only).

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ function tsify(b, opts) {
 	var tsifier = new Tsifier(opts);
 	tsifier.on('error', function (error) { b.emit('error', error); });
 
+	var files = [];
+
+	b.on('file', function(f) {
+		files.push(f);
+	});
+
 	b.on('bundle', function () {
 		tsifier.clearCompilationCache();
-		tsifier.compileAndCacheFiles((b._entries || b._options.entries).filter(Tsifier.isTypescript));
+		tsifier.compileAndCacheFiles((b._entries || b._options.entries || files).filter(Tsifier.isTypescript));
 	});
 
 	b.transform(tsifier.transform.bind(tsifier));

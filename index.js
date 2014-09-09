14241ed291f3af9d4673ae45e17fef9790d52d2f
Greg Smith <smr.kyu@gmail.com>
2014-09-09 13:47:42 -0500

    Fixed race condition causing terrible performance.

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 var Tsifier = require('./lib/Tsifier');
+var through = require('through2');
 
 function tsify(b, opts) {
 	var tsifier = new Tsifier(opts);
 	tsifier.on('error', function (error) { b.emit('error', error); });
 
-	var files = [];
-
-	b.on('file', function(f) {
-		files.push(f);
-	});
-
-	b.on('bundle', function () {
-		tsifier.clearCompilationCache();
-		tsifier.compileAndCacheFiles((b._entries || b._options.entries || files).filter(Tsifier.isTypescript));
-	});
-
+	setupPipeline();
 	b.transform(tsifier.transform.bind(tsifier));
 
-	if (b._extensions.indexOf('.ts') === -1)
-		b._extensions.unshift('.ts');
+	b.on('reset', function () {
+		tsifier.clearCompilationCache();
+		setupPipeline();
+	});
+
+	function setupPipeline() {
+		if (b._extensions.indexOf('.ts') === -1)
+			b._extensions.unshift('.ts');
+
+		b.pipeline.get('record').push(gatherDeps(function (files) {
+			tsifier.compileAndCacheFiles(files);
+		}));
+	}
 };
 
 module.exports = tsify;
+
+function gatherDeps(cb) {
+	var rows = [];
+	return through.obj(transform, flush);
+
+	function transform(row, enc, next) {
+		rows.push(row);
+		next();
+	}
+
+	function flush(next) {
+		cb(rows.map(function (row) { return row.id; }));
+		rows.forEach(this.push.bind(this));
+		this.push(null);
+		next();
+	}
+}

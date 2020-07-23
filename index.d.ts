6f502d0d3471c4e2bb0ee1a148cdbd3b534eafcd
Brian J Brennan <brianloveswords@gmail.com>
2020-07-23 17:35:45 -0400

    Fix TypeScript types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,11 @@
 import { BrowserifyObject, CustomOptions } from "browserify";
-import { CompilerOptions, ModuleKind, ScriptTarget } from "typescript";
+import typescript, { CompilerOptions, ModuleKind, ScriptTarget } from "typescript";
 
-export interface Options extends CustomOptions, CompilerOptions {
-	typescript?: string | import("typescript");
+// Provide local definition of Omit for compatibility with TypeScript <3.5
+type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+export interface Options extends CustomOptions, Omit<CompilerOptions, "project"> {
+	typescript?: string | typeof typescript;
 	global?: boolean;
 	m?: ModuleKind;
 	p?: string | CompilerOptions;
@@ -10,6 +13,4 @@ export interface Options extends CustomOptions, CompilerOptions {
 	t?: ScriptTarget;
 }
 
-function tsify(b: BrowserifyObject, opts: Options): any;
-
-export = tsify;
+export default function tsify(b: BrowserifyObject, opts: Options): any;
